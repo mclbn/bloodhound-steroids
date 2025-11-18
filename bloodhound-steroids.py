d6f4a820fb5aa3173a2033540be13be618d7f110
Marc Lebrun <marc@pentest>
2025-11-18 16:22:51 +0100

    New relationship : SameLocalAdmin

--- bloodhound-steroids.py
+++ bloodhound-steroids.py
@@ -7,10 +7,11 @@ import sys
 from neo4j import GraphDatabase
 
 known_modules = [
-    'samepass'
+    'samepass',
+    'samelocaladmin'
 ]
 
-def do_insert(driver, user, others):
+def do_insert_samepassword(driver, user, others):
     res_count = 0
     query = """
     MATCH (a:User), (b:User)
@@ -48,7 +49,7 @@ def do_samepass_user_file(driver, user_files):
     users = list(set(users))
 
     for user in users:
-        updated += do_insert(driver, '%s@%s' % (user, options.domain),
+        updated += do_insert_samepassword(driver, '%s@%s' % (user, options.domain),
                              ['%s@%s' % (x, options.domain) for x in users if x != user])
 
     return updated
@@ -92,15 +93,13 @@ def do_samepass_nt_file(driver, nt_file):
 
     for user_list in final:
         for user in user_list:
-            updated += do_insert(driver, '%s@%s' % (user, options.domain),
+            updated += do_insert_samepassword(driver, '%s@%s' % (user, options.domain),
                              ['%s@%s' % (x, options.domain) for x in user_list if x != user])
 
     return updated
 
 def do_samepass(driver, options):
     updated = 0
-
-    print(options)
 
     if options.user_file is not None:
         updated += do_samepass_user_file(driver, options.user_file)
@@ -109,19 +108,74 @@ def do_samepass(driver, options):
 
     print('Updated : %d\n' % updated)
 
+def do_insert_samelocal_admin(driver, computer, others):
+    res_count = 0
+    query = """
+    MATCH (a:Computer), (b:Computer)
+    WHERE a.name = toUpper("%s")
+    AND b.name = toUpper("%s")
+    CREATE (a)-[r:SameLocalAdmin]->(b), (b)-[k:SameLocalAdmin]->(a)
+    RETURN r
+    """
+
+    for other in others:
+        with driver.session() as session:
+            try:
+                print(query % (computer, other))
+                result = session.run(query % (computer, other))
+                res_count = len(result.value())
+            except Exception as e:
+                print(str(e))
+    return res_count
+
+def do_samelocaladmin(driver, options):
+    computers = []
+    updated = 0
+
+    for computer_file in options.computer_file:
+        try:
+            f = open(computer_file)
+        except Exception as e:
+            print(str(e))
+        else:
+            with f:
+                for line in f:
+                    clean = line.rstrip()
+                    if len(clean):
+                        computers.append(clean)
+
+    computers = list(set(computers))
+
+    for computer in computers:
+        updated += do_insert_samelocal_admin(driver, '%s.%s' % (computer, options.domain),
+                             ['%s.%s' % (x, options.domain) for x in computers if x != computer])
+    print('Updated : %d\n' % updated)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(add_help = True, description = "Give performance enhancing drugs to bloodhound.")
+    parser = argparse.ArgumentParser(
+        add_help = True,
+        description = "Give performance enhancing drugs to bloodhound.")
 
-    parser.add_argument('-l', '--list', action='store_true', default=False, help='list available modules')
-    parser.add_argument('-m', '--module', action='store', help='module to use')
-    parser.add_argument('--domain', action='store', help='the related domain')
-    parser.add_argument('--user-file', action='append', help='a list of user names (you can specify multiple files)')
-    parser.add_argument('--nt-file', action='store', help='a NT dump file (NTDS.DIT) to parse')
-    parser.add_argument('--neo4j-host', '-n', action='store', help='target neo4j host')
-    parser.add_argument('--port', '-p', action='store', default=7687, help='target neo4j port (default: 7474)')
-    parser.add_argument('--user', '-u', action='store', default='neo4j', help='target neo4j username')
-    parser.add_argument('--secret', '-s', action='store', help='target neo4j secret')
+    parser.add_argument('-l', '--list', action='store_true', default=False,
+                        help='list available modules')
+    parser.add_argument('-m', '--module', action='store',
+                        help='module to use')
+    parser.add_argument('--domain', action='store',
+                        help='the related domain')
+    parser.add_argument('--user-file', action='append',
+                        help='a list of user names (you can specify multiple files)')
+    parser.add_argument('--computer-file', action='append',
+                        help='a list of computer names (you can specify multiple files)')
+    parser.add_argument('--nt-file', action='store',
+                        help='a NT dump file (NTDS.DIT) to parse')
+    parser.add_argument('--neo4j-host', '-n', action='store',
+                        help='target neo4j host')
+    parser.add_argument('--port', '-p', action='store', default=7687,
+                        help='target neo4j port (default: 7474)')
+    parser.add_argument('--user', '-u', action='store', default='neo4j',
+                        help='target neo4j username')
+    parser.add_argument('--secret', '-s', action='store',
+                        help='target neo4j secret')
 
     if len(sys.argv) == 1:
         parser.print_help()
@@ -170,5 +224,13 @@ if __name__ == '__main__':
         if options.user_file is not None and options.nt_file is not None:
             print('You cannot specify both user list(s) and a dump file. Choose one.')
             sys.exit(1)
-
         do_samepass(driver, options)
+
+    if options.module == 'samelocaladmin':
+        if options.domain is None or len(options.domain) == 0:
+            print('You must specify a domain.')
+            sys.exit(1)
+        if options.computer_file is None:
+            print('You must specify a computer list.')
+            sys.exit(1)
+        do_samelocaladmin(driver, options)
